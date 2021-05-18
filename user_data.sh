fd6a45d9744a66613a643aba1bc5b82b96e8fc42
m-ushatov <m.ushatov98@gmail.com>
2021-05-18 14:34:49 +0300

    testing git trigger

--- user_data.sh
+++ user_data.sh
@@ -5,3 +5,4 @@ sudo amazon-linux-extras install docker -y
 sudo service docker start
 sudo usermod -aG docker ec2-user
 sudo docker login --username AWS --password-stdin 466781822523.dkr.ecr.us-east-1.amazonaws.com
+#------------
