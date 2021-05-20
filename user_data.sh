71403c8da8d7d6fa3cc0c4b54d6cf6e6a2c27288
m-ushatov <m.ushatov98@gmail.com>
2021-05-20 12:03:54 +0300

    login->run

--- user_data.sh
+++ user_data.sh
@@ -4,4 +4,4 @@ sudo yum update -y
 sudo amazon-linux-extras install docker -y
 sudo service docker start
 sudo usermod -aG docker ec2-user
-sudo docker login --username AWS --password-stdin 466781822523.dkr.ecr.us-east-1.amazonaws.com
+sudo docker run hello-world
