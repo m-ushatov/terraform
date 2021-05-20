0bffb16754d7ea18dd8b4f636b84d3e813fcbd22
m-ushatov <m.ushatov98@gmail.com>
2021-05-20 16:13:58 +0300

    create ecr_repo

--- docker.tf
+++ docker.tf
@@ -7,3 +7,12 @@ resource "aws_instance" "my_amazon_docker" {
   instance_type = "t2.micro"
   user_data = file("user_data.sh")
 }
+
+resource "aws_ecr_repository" "ecr_repo" {
+  name                 = "ecr_repo_dt"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
