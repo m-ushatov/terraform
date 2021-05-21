4774d1d5476b1519b1281f1b4e86d04bf4bb83ee
m-ushatov <m.ushatov98@gmail.com>
2021-05-21 11:49:23 +0300

    try to login aws ecr

--- user_data.sh
+++ user_data.sh
@@ -4,4 +4,4 @@ sudo yum update -y
 sudo amazon-linux-extras install docker -y
 sudo service docker start
 sudo usermod -aG docker ec2-user
-sudo docker run hello-world
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 215132563327.dkr.ecr.us-east-2.amazonaws.com
